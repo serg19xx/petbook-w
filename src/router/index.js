c5954ceaf6dff01fa22f97e8d508b3d005b83fca
Serg Kostyuk <serg.kostyuk@gmail.com>
2024-12-14 14:05:39 -0500

    Added .nojeykyll to dist

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ProfileView from '../views/ProfileView.vue'
 import AboutView from '../views/AboutView.vue'
 import PetGalleryView from '../views/PetGalleryView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL), // import.meta.env.BASE_URL
+  history: createWebHashHistory(import.meta.env.BASE_URL), // import.meta.env.BASE_URL
   routes: [
     {
       path: '/',
