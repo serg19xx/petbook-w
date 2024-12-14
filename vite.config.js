04632f26e5e35db23914e7474829b96a00ee5bd9
Serg Kostyuk <serg.kostyuk@gmail.com>
2024-12-14 13:31:10 -0500

    Change imports in router index.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',//process.env.NODE_ENV === 'production' ? '/' : '/',  ////publicPath
+  publicPath: '/',//process.env.NODE_ENV === 'production' ? '/' : '/',  ////publicPath
   plugins: [
     vue(),
     vueDevTools(),
