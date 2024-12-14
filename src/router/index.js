27dd73ce8449f33ba3bf709b0817f0d0badda0e9
Serg Kostyuk <serg.kostyuk@gmail.com>
2024-12-14 12:55:15 -0500

    Change imports in router index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import ProfileView from '@/views/ProfileView.vue'
-import AboutView from '@/views/AboutView.vue'
-import PetGalleryView from '@/views/PetGalleryView.vue'
+import ProfileView from '../views/ProfileView.vue'
+import AboutView from '../views/AboutView.vue'
+import PetGalleryView from '../views/PetGalleryView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL), // import.meta.env.BASE_URL
